8aaa237b86683056d72db1063dc94a193545a3e1
richardtrisnosaputra <richardtrisnosaputra@gmail.com>
2023-09-11 12:09:16 +0700

    adding app log

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+const fs = require('fs')
+const applog = new console.Console(fs.createWriteStream('./app.log'))
 
 app.get('/', function(req, res){
-   res.send("Hello world!");
+    applog.log('Hello world!')
+    res.send("Hello world!")
 });
 
 app.listen(3210, () => {
-    console.log('Running on port 3210')
+    applog.log('Running on port 3210')
 });
