b8e2facf2019f138c355e3cf54bd006db17df6b6
richardtrisnosaputra <richardtrisnosaputra@gmail.com>
2023-09-23 15:36:14 +0700

    adding newrelic and docker

--- index.js
+++ index.js
@@ -3,9 +3,40 @@ var app = express()
 const fs = require('fs')
 const applog = new console.Console(fs.createWriteStream('./app.log'))
 
-app.get('/', function(req, res){
-    applog.log('Hello world!')
-    res.send("Hello world!")
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+// app.get('/', function(req, res){
+//     applog.log('making 32 random character')
+//     res.send(makeid(32))
+// });
+
+app.get('/', function(req, res) {
+    const response = 'EɝU�O��X�%��Az0=Door 1&1=Door 2';
+    const charIndex = response.indexOf('=');
+    const responseClean = response.substring(charIndex-1);
+    const responseArray = responseClean.split('&');
+    const data = [];
+    for(let i = 0; i < responseArray.length; i++) {
+        const dataArray = responseArray[i].split('=');
+        data.push({
+            id: dataArray[0],
+            name: dataArray[1]
+        });
+    }
+    res.send({
+        status: 200,
+        data: data
+    });
 });
 
 app.listen(3210, () => {
